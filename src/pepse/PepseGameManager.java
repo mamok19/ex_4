900d9c364ca0f9ace5c5753ca773984de24572e6
Eilam Soroka <eilamsoroka@gmail.com>
2025-12-28 19:29:25 +0200

    implemente sky block and terrain and deciding the top layer as topGroundBlock and the else lowerGroundBlock

--- src/pepse/PepseGameManager.java
+++ src/pepse/PepseGameManager.java
@@ -1,20 +1,50 @@
 package pepse;
+import danogl.GameObject;
+import danogl.collisions.Layer;
+import pepse.world.Block;
+import pepse.world.Sky;
 
 import danogl.GameManager;
 import danogl.gui.ImageReader;
 import danogl.gui.SoundReader;
 import danogl.gui.UserInputListener;
 import danogl.gui.WindowController;
+import pepse.world.Terrain;
 
+/**
+ * The main class of the Pepse game.
+ * incharge of initializing and running the game.
+ * @author Eilam Soroka, Maayan Felig
+ */
 public class PepseGameManager extends GameManager {
 
-    @Override
-    public void initializeGame(ImageReader imageReader, SoundReader soundReader, UserInputListener inputListener, WindowController windowController) {
-        super.initializeGame(imageReader, soundReader, inputListener, windowController);
+    private static final int SKY_LAYER = -250;
+    private static final int SEED = 73;
 
+    @Override
+    public void initializeGame(ImageReader imageReader, SoundReader soundReader, UserInputListener inputListener,
+                               WindowController windowController) {
+        super.initializeGame(imageReader, soundReader, inputListener, windowController);
+        initializeSky(windowController);
+        initializeTerrain(windowController);
+    }
+
+    private void initializeTerrain(WindowController windowController) {
+        Terrain terrain = new Terrain(windowController.getWindowDimensions(), SEED);
+        for (Block block : terrain.createInRange(0, (int) windowController.getWindowDimensions().x())) { //todo change range
+            if (block.getTag() == "topGroundBlock"){
+                gameObjects().addGameObject(block, Layer.STATIC_OBJECTS);
+                continue;
+            }
+            gameObjects().addGameObject(block, Layer.STATIC_OBJECTS);
+        }
     }
 
     public static void main(String[] args) {
-        new GameManager().run();
+        new PepseGameManager().run();
+    }
+
+    private void initializeSky(WindowController windowController) {
+        gameObjects().addGameObject(Sky.create(windowController.getWindowDimensions()), SKY_LAYER);
     }
 }
--- src/pepse/world/Terrain.java
+++ src/pepse/world/Terrain.java
@@ -1,4 +1,64 @@
 package pepse.world;
 
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
+import pepse.utils.NoiseGenerator;
+import danogl.gui.rendering.RectangleRenderable;
+import pepse.utils.ColorSupplier;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+/**
+ * Represents the terrain of the game world.
+ * The terrain's height is determined using a noise generator to create a natural, uneven surface.
+ * @author Eilam Soroka, Maayan Felig
+ */
 public class Terrain {
+    private static final int START_x0 = 0;
+    private static final double FACTOR_TERRAIN = 7;
+    private static final double NOISE_FACTOR_TERRAIN = Block.SIZE * FACTOR_TERRAIN;
+    private static final Color BASE_GROUND_COLOR = new Color(212, 123, 74);
+    private static final int TERRAIN_DEPTH = 20;
+    private final Vector2 windowDimensions;
+    private final float groundHeightAtX0;
+    private final int seed;
+    private final NoiseGenerator noiseGenerator;
+
+    public Terrain(Vector2 windowDimensions, int seed){
+        this.windowDimensions = windowDimensions;
+        this.seed = seed;
+        //Random rand = new Random(Objects.hash(START_x0, seed));
+        //this.groundHeightAtX0 = rand.nextInt((int)(windowDimensions.y()*(2/3.0)), (int)(windowDimensions.y()*(5/6.0)));
+        this.groundHeightAtX0 = (2/3.0f) * windowDimensions.y();
+        this.noiseGenerator = new NoiseGenerator(seed, (int)groundHeightAtX0);
+    }
+    public float groundHeightAt(float x){
+        float noise = (float) noiseGenerator.noise(x,NOISE_FACTOR_TERRAIN);
+        return groundHeightAtX0 + noise;
+    }
+
+    public List<Block> createInRange(int minX, int maxX) {
+        
+        Block block = new Block(Vector2.ZERO,
+                new RectangleRenderable(ColorSupplier.approximateColor(BASE_GROUND_COLOR)));
+        ArrayList<Block> blocks = new ArrayList<>();
+        for (int x = minX; x <= maxX; x += Block.SIZE) {
+            float groundHeight = groundHeightAt(x);
+            Block topBlock = new Block(new Vector2(x, (int) groundHeight),
+                    new RectangleRenderable(ColorSupplier.approximateColor(BASE_GROUND_COLOR)));
+            topBlock.setTag("topGroundBlock");
+            blocks.add(topBlock);
+            for (int y = (int) (groundHeight + Block.SIZE);
+                 y < (int) groundHeight + (TERRAIN_DEPTH * Block.SIZE); y += Block.SIZE) {
+                Block newBlock = new Block(new Vector2(x, y),
+                        new RectangleRenderable(ColorSupplier.approximateColor(BASE_GROUND_COLOR)));
+                newBlock.setTag("lowerGroundBlock");
+                blocks.add(newBlock);
+            }
+        }
+        return blocks;
+    }
+
 }
