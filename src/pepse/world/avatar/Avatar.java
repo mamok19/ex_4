63ad3de16de72790f0efaa2b5075ce2db5ddac15
Eilam Soroka <eilamsoroka@gmail.com>
2025-12-30 12:24:16 +0200

    finished the animation part

--- src/pepse/world/avatar/Avatar.java
+++ src/pepse/world/avatar/Avatar.java
@@ -5,6 +5,7 @@ import danogl.collisions.Collision;
 import danogl.gui.ImageReader;
 import danogl.gui.UserInputListener;
 import danogl.util.Vector2;
+import danogl.gui.rendering.AnimationRenderable;
 
 import java.awt.event.KeyEvent;
 
@@ -25,7 +26,13 @@ public class Avatar extends GameObject {
     private static final int DOUBLE_JUMP_COST = 50;
     private static final float ENERGY_RETURN_RATE = 1f; //todo write on readme that i change movement cost
     private static final int ENERGY_RETURN_AMOUNT = 1;
-
+    private static final String[] IDLE_IMAGES = new String[]
+    {"assets/idle_0.png","assets/idle_1.png","assets/idle_2.png","assets/idle_3.png"};
+    private static final String[] WALKING_IMAGES = new String[]
+            {"assets/run_0.png","assets/run_1.png","assets/run_2.png","assets/run_3.png"};
+    private static final String[] JUMPING_IMAGES = new String[]
+            {"assets/jump_0.png","assets/jump_1.png","assets/jump_2.png","assets/jump_3.png"};
+    private static final double TIME_BETWEEN_ANIMATION_FRAMES = 0.2f;
 
 
     private static enum state {
@@ -42,7 +49,10 @@ public class Avatar extends GameObject {
     private final Vector2 topleftCorner;
     private final UserInputListener inputListener;
     private final ImageReader imageReader;
-    private final state currentState;
+    private state currentState;
+    private AnimationRenderable idleAnimation;
+    private AnimationRenderable walkAnimation;
+    private AnimationRenderable jumpAnimation;
 
     private int energyMeter;
     private boolean spaceWasPressed = false;
@@ -60,9 +70,16 @@ public class Avatar extends GameObject {
         transform().setAccelerationY(GRAVITY);
         this.currentState = state.IDLE;
         this.energyMeter = MAX_ENERGY;
-
-
         this.doubleJumpUsed = false;
+
+        this.idleAnimation = new AnimationRenderable(IDLE_IMAGES
+                , imageReader, true, TIME_BETWEEN_ANIMATION_FRAMES);
+        this.walkAnimation = new AnimationRenderable(WALKING_IMAGES
+                , imageReader, true, TIME_BETWEEN_ANIMATION_FRAMES);
+        this.jumpAnimation = new AnimationRenderable(JUMPING_IMAGES
+                , imageReader, true, TIME_BETWEEN_ANIMATION_FRAMES);
+
+        renderer().setRenderable(idleAnimation);
 
         setTag(AVATAR_TAG);
     }
@@ -71,6 +88,7 @@ public class Avatar extends GameObject {
     public void update(float deltaTime) {
         super.update(deltaTime);
         handleMovement();
+        updateState();
 
     }
 
@@ -83,6 +101,38 @@ public class Avatar extends GameObject {
                 energyReturn = 0f;
                 energyMeter += ENERGY_RETURN_AMOUNT;
             }
+        }
+    }
+
+    private void updateState() {
+        if (!onGround) {
+            setState(state.JUMPING);
+        }
+        else if (transform().getVelocity().x() != 0) {
+            setState(state.WALKING);
+        }
+        else {
+            setState(state.IDLE);
+        }
+    }
+
+    private void setState(state newState) {
+        if (newState == currentState) {
+            return;
+        }
+
+        currentState = newState;
+
+        switch (currentState) {
+            case IDLE:
+                renderer().setRenderable(idleAnimation);
+                break;
+            case WALKING:
+                renderer().setRenderable(walkAnimation);
+                break;
+            case JUMPING:
+                renderer().setRenderable(jumpAnimation);
+                break;
         }
     }
 
@@ -127,7 +177,12 @@ public class Avatar extends GameObject {
                 wantsToMove = false;
             }
         }
-
+        if (velocityX > 0) {
+            renderer().setIsFlippedHorizontally(false);
+        }
+        else if (velocityX < 0) {
+            renderer().setIsFlippedHorizontally(true);
+        }
         transform().setVelocityX(velocityX);
         return wantsToMove;
     }
