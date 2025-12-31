8bf2c2a0b5be700841468acdd9e38753851a42b8
mfelig <mfelig@gmail.com>
2025-12-31 10:50:33 +0200

    built Leaf class

--- src/pepse/world/Block.java
+++ src/pepse/world/Block.java
@@ -8,9 +8,15 @@ import pepse.Constants;
 
 public class Block extends GameObject {
 
+    private static final int BLOCK_SIZE = 30;
+
     public Block(Vector2 topLeftCorner, Renderable renderable) {
         super(topLeftCorner, Vector2.ONES.mult(Constants.BLOCK_SIZE), renderable);
         physics().preventIntersectionsFromDirection(Vector2.ZERO);
         physics().setMass(GameObjectPhysics.IMMOVABLE_MASS);
+    }
+
+    public int getSize() {
+        return BLOCK_SIZE;
     }
 }
--- src/pepse/world/trees/Leaf.java
+++ src/pepse/world/trees/Leaf.java
@@ -1,5 +1,7 @@
 package pepse.world.trees;
 
+import danogl.components.ScheduledTask;
+import danogl.components.Transition;
 import danogl.gui.rendering.RectangleRenderable;
 import danogl.util.Vector2;
 import pepse.utils.ColorSupplier;
@@ -14,8 +16,57 @@ import java.awt.*;
  */
 public class Leaf extends Block {
     private static final Color BASE_LEAF_COLOR = new Color(50, 200, 30);;
+    private static final String LEAF_TAG = "leaf";
+    private static final float LEAF_CHANGE_TIME = 2.0f;
+    private static final float LEAF_ANGLE_DIFF = 20.0f;
+    private static final float LEAF_SIZE_FACTOR = 2.0f;
 
+    /**
+     * Constructor for a Leaf object that moves with the wind.
+     * @param TopLeftCorner top left corner where the leaf should appear on screen
+     */
     public Leaf(Vector2 TopLeftCorner) {
         super(TopLeftCorner, new RectangleRenderable(ColorSupplier.approximateColor(BASE_LEAF_COLOR)));
+        this.setTag(LEAF_TAG);
+        physics().preventIntersectionsFromDirection(Vector2.ZERO);
+    }
+
+    /**
+     * schedules the movement of a leaf in both its angle and dimensions
+     * @param time the amount of time to wait before starting movement
+     */
+    public void createWindEffect(float time) {
+        new ScheduledTask(this, time,true, () -> {
+            changeLeafAngle();
+            changeLeafDimensions();
+        });
+    }
+
+    private void changeLeafAngle() {
+        float startAngle = this.renderer().getRenderableAngle();
+        float endAngle = startAngle + LEAF_ANGLE_DIFF;
+        new Transition<>(this,
+                (Float angle) -> this.renderer().setRenderableAngle(angle),
+                startAngle,
+                endAngle,
+                Transition.LINEAR_INTERPOLATOR_FLOAT,
+                LEAF_CHANGE_TIME,
+                Transition.TransitionType.TRANSITION_BACK_AND_FORTH,
+                null);
+    }
+
+    private void changeLeafDimensions() {
+        Vector2 startSize = new Vector2(this.getSize(), this.getSize());
+        Vector2 endSize = startSize.subtract(
+                new Vector2((float) this.getSize()/LEAF_SIZE_FACTOR,
+                0));
+        new Transition<>(this,
+                this::setDimensions,
+                startSize,
+                endSize,
+                Transition.LINEAR_INTERPOLATOR_VECTOR,
+                LEAF_CHANGE_TIME,
+                Transition.TransitionType.TRANSITION_BACK_AND_FORTH, null
+                );
     }
 }
